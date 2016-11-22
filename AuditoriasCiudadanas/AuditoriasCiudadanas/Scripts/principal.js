0d7979263e53a515dc66eaa2638dcfb1120eb959
dvillamizar <villamizardiana@gmail.com>
2016-11-21 21:42:14 -0500

    prueba_cambios

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Scripts/principal.js
@@ -0,0 +1,9 @@
+﻿//validación de correo electrónico
+function validaEmail(cadena) {
+    if (cadena.match(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
