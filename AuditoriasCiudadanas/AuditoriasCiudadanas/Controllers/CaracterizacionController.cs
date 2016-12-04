a20d3a1a1f9f0dcc3a9791743cf84bcd5dde6a23
Diana Bautista <Diana Bautista@HOME>
2016-12-03 21:44:23 -0500

    Se sube el formato encuesta caracterización.

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/CaracterizacionController.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+
+namespace AuditoriasCiudadanas.Controllers
+{
+  public class CaracterizacionController
+  {
+    /// <summary>
+    /// Esta función se utiliza para consultar todos los municipios y su departamento
+    /// </summary>
+    /// <returns>Devuelve un texto con el listado de todos los municipios y su departamento que se encuentren en la base de datos siguiendo la notación Municipio - Departamento.
+    /// Se emplea el caracter \n para separar a un municipio de otro.
+    /// </returns>
+    public string ObtenerMunicipiosYDepartamentos()
+    {
+      string rta = string.Empty;
+      DataTable dtSalida = Models.clsCaracterizacionModels.GetMunicipiosDepartamento();
+      if (dtSalida != null) //Se valida que la consulta de la base de datos venga con datos
+        foreach (DataRow drFila in dtSalida.Rows) //Cada fila corresponde a un registro con el nombre del municipio y su departamento.
+          if (drFila != null && drFila.ItemArray.Length >= 2 && drFila[1].ToString() != string.Empty)
+            rta = rta == string.Empty ? drFila[1].ToString() + " - " + drFila[0].ToString() : rta + "\n" + drFila[1].ToString() + " - " + drFila[0].ToString();
+      return rta;
+    }
+    /// <summary>
+    /// Sirve para validar la existencia del municipio y departamento en la tabla Divipola
+    /// </summary>
+    /// <param name="nombreMunicipio">Corresponde a la combinación municipio - departamento</param>
+    /// <returns></returns>
+    public bool ExisteMunicipio(string nombreMunicipio)
+    {
+      var parametros = nombreMunicipio.Split('*');//El * es un caracter que usamos para separar los datos de los dos formularios de la encuesta
+      return Models.clsCaracterizacionModels.ExisteMunicipio(parametros[0]);
+    }
+    /// <summary>
+    /// Sirve para guardar los resultados de la encuesta de caracterización.
+    /// </summary>
+    /// <param name="parametrosGuardar">Corresponde a la lista de parámetros de la encuesta que serán guardados</param>
+    /// <returns></returns>
+    public bool GuardarEncuestaCaracterizacion(string parametrosGuardar)
+    {
+      var parametos = parametrosGuardar.Split('*');//El * es un caracter que usamos para separar los datos de los dos formularios de la encuesta
+      return Models.clsCaracterizacionModels.IngresarEncuesta(parametos)== "1<||>"? true:false;
+    }
+  }
+}
---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Models/clsCaracterizacion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+
+namespace AuditoriasCiudadanas.Models
+{
+  static public class clsCaracterizacionModels
+  {
+    static string cadTransparencia = ConfigurationManager.ConnectionStrings["Transparencia"].ConnectionString;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    static public DataTable GetMunicipiosDepartamento()
+    {
+      return DbManagement.getDatosDataTable("dbo.pa_listar_depmun", CommandType.StoredProcedure, cadTransparencia, new List<PaParams>());
+    }
+
+    static private int ObtenerIdDivipola(string municipioDepartamento)
+    {
+      var entidadesTerritoriales = municipioDepartamento.Split('-');
+      if (entidadesTerritoriales.Length < 2) return -1;//Significa que no existen los datos de entrada (Municipio y Departamento)
+      var parametros = new List<PaParams>();
+      parametros.Add(new PaParams("@nombreCiudad", SqlDbType.VarChar, entidadesTerritoriales[0].Trim(), ParameterDirection.Input, 100));
+      parametros.Add(new PaParams("@nombreDepto", SqlDbType.VarChar, entidadesTerritoriales[1].Trim(), ParameterDirection.Input, 100));
+      var dtIdDivipola = DbManagement.getDatosDataTable("dbo.pa_obt_divipolaid", CommandType.StoredProcedure, cadTransparencia, parametros);
+      if (dtIdDivipola != null && dtIdDivipola.Rows.Count > 0)
+      {
+        int rta = 0;
+        return int.TryParse(dtIdDivipola.Rows[0].ItemArray[0].ToString(), out rta) ? rta : -2;//Significa que el valor que trajo no es entero
+      }
+      else return 0;//Significa que no hubo datos
+    }
+    /// <summary>
+    /// Sirve para guardar los datos de la encuesta
+    /// </summary>
+    /// <param name="parametrosGuardar">Son los parámetros de la encuesta</param>
+    /// <returns>Devuelve un texto indicando si hubo o no errores al guardar la encuesta</returns>
+    static public string IngresarEncuesta(params object[] parametrosGuardar)
+    {
+      if (parametrosGuardar == null || parametrosGuardar.Length <= 16) return "-2";//Significa que los parámetros no son correctos
+      var idDivipola = ObtenerIdDivipola(parametrosGuardar[0].ToString());
+      if (idDivipola <= 0) return "-2";
+      var vinculacionActual = parametrosGuardar[9].ToString() != string.Empty ? parametrosGuardar[9].ToString() : parametrosGuardar[8].ToString();
+      var mecanismoHaParticipado = parametrosGuardar[11].ToString() != string.Empty ? parametrosGuardar[11].ToString() : parametrosGuardar[10].ToString();
+      var espaciosParticipadoCiudadano = parametrosGuardar[13].ToString() != string.Empty ? parametrosGuardar[13].ToString() : parametrosGuardar[12].ToString();
+      List <DataTable> Data = new List<DataTable>();
+      List<PaParams> parametros = new List<PaParams>();
+      string cod_error = "1";
+      string mensaje_error = string.Empty;
+      parametros.Add(new PaParams("@IdUsuario", SqlDbType.VarChar, parametrosGuardar[16].ToString(), ParameterDirection.Input, 100));
+      parametros.Add(new PaParams("@idDivipola", SqlDbType.VarChar, idDivipola.ToString(), ParameterDirection.Input, 15));
+      parametros.Add(new PaParams("@Fecha", SqlDbType.DateTime, DateTime.Now, ParameterDirection.Input));
+      parametros.Add(new PaParams("@Genero", SqlDbType.VarChar, parametrosGuardar[1].ToString(), ParameterDirection.Input, 50));
+      parametros.Add(new PaParams("@RangoEdad", SqlDbType.VarChar, parametrosGuardar[2].ToString(), ParameterDirection.Input, 20));
+      parametros.Add(new PaParams("@Ocupacion", SqlDbType.VarChar, parametrosGuardar[3].ToString(), ParameterDirection.Input, 200));
+      parametros.Add(new PaParams("@Cargo", SqlDbType.VarChar, parametrosGuardar[4].ToString(), ParameterDirection.Input,50));
+      parametros.Add(new PaParams("@LugarResidencia", SqlDbType.VarChar, parametrosGuardar[5].ToString(), ParameterDirection.Input,50));
+      parametros.Add(new PaParams("@PerteneceMinoria", SqlDbType.VarChar, parametrosGuardar[6].ToString(), ParameterDirection.Input,2));
+      parametros.Add(new PaParams("@PerteneceOrganizacionSocial", SqlDbType.VarChar, parametrosGuardar[7].ToString(), ParameterDirection.Input,2));
+      parametros.Add(new PaParams("@VinculacionActual", SqlDbType.VarChar, vinculacionActual, ParameterDirection.Input,100));
+      parametros.Add(new PaParams("@MecanismoHaParticipado", SqlDbType.VarChar, mecanismoHaParticipado, ParameterDirection.Input,50));
+      parametros.Add(new PaParams("@EspaciosParticipadoCiudadano", SqlDbType.VarChar, espaciosParticipadoCiudadano, ParameterDirection.Input,50));
+      parametros.Add(new PaParams("@RecursosAlcaldia", SqlDbType.VarChar, parametrosGuardar[14].ToString(), ParameterDirection.Input,10));
+      parametros.Add(new PaParams("@AuditoriasVisiblesDNP", SqlDbType.VarChar, parametrosGuardar[15].ToString(), ParameterDirection.Input));
+      parametros.Add(new PaParams("@cod_error", SqlDbType.Int, cod_error, ParameterDirection.Output));
+      parametros.Add(new PaParams("@mensaje_error", SqlDbType.VarChar, mensaje_error, ParameterDirection.Output));
+      Data = DbManagement.getDatos("dbo.pa_ins_encaracterizacion", CommandType.StoredProcedure, cadTransparencia, parametros);
+      return cod_error + "<||>" + mensaje_error;
+    }
+      /// <summary>
+      /// Sirve para validar si el municipio existe en la base de datos 
+      /// </summary>
+      /// <param name="nombreMunicipio">Correponde a la secuencia municipio-Departamento</param>
+      /// <returns>Si los nombres del municipio y departamento coinciden con los almacenados en la bd devuelve verdadero.</returns>
+      static public bool ExisteMunicipio(string nombreMunicipio)
+    {
+      var municipio = string.Empty;
+      var departamento = string.Empty;
+      var municipioProcesar = nombreMunicipio.Split('-');
+      if(municipioProcesar.Length>1)
+      {
+        municipio = municipioProcesar[0].Trim().ToUpper();
+        departamento= municipioProcesar[1].Trim().ToUpper();
+      }
+      else municipio = municipioProcesar[0].Trim().ToUpper();
+      DataTable dtMunicipios= DbManagement.getDatosDataTable("dbo.pa_listar_depmun", CommandType.StoredProcedure, cadTransparencia, new List<PaParams>());
+      var existeMunicipio = (from registro in dtMunicipios.AsEnumerable()
+                            where registro.ItemArray[1] != null && registro.ItemArray[0] != null && registro.ItemArray[1].ToString().Trim().ToUpper() == municipio && registro.ItemArray[0].ToString().Trim().ToUpper() == departamento
+                            select registro).ToArray();
+      return existeMunicipio.Any();
+    }
+  }
+
+}
