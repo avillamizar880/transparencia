70d054d5aa3281130826825bb19a61dd3d9076c0
dvillamizar <villamizardiana@gmail.com>
2016-12-05 11:28:36 -0500

    add_actas

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Audiencias/ActaReuniones.aspx.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Audiencias/ActaReuniones.aspx.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Collections.Specialized;
 
 namespace AuditoriasCiudadanas.Views.Audiencias
 {
     public partial class ActaReuniones : System.Web.UI.Page
     {
+        public override void VerifyRenderingInServerForm(System.Web.UI.Control control)
+        {
+            //No obliga a a la página a tener un form incluido
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (HttpContext.Current.Request.HttpMethod == "POST")
+            {
+                string tipo_audiencia = "0";
+                NameValueCollection pColl = Request.Params;
+                if (pColl.AllKeys.Contains("tipo_audiencia"))
+                {
+                    tipo_audiencia = Request.Params.GetValues("tipo_audiencia")[0].ToString();
+                }
 
+
+
+            }
         }
     }
 }
