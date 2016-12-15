8395b6f075c172ae2c5ac744d077c541e40fe846
dvillamizar <villamizardiana@gmail.com>
2016-12-14 22:37:23 -0500

    registroCompromisos

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Audiencias/RegistrarCompromisos.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AuditoriasCiudadanas.Views.Audiencias
+{
+    public partial class RegistrarCompromisos : System.Web.UI.Page
+    {
+        public override void VerifyRenderingInServerForm(System.Web.UI.Control control)
+        {
+            //No obliga a a la página a tener un form incluido
+        }
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
