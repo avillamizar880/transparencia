63a69502cc1f2bc241d0af4aa1d3c53785833b14
asus1 <asus1@ASUS>
2016-12-18 19:38:14 -0500

    Merge

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/PlanTrabajoController.cs
@@ -0,0 +1,25 @@
+﻿using AuditoriasCiudadanas.Models;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace AuditoriasCiudadanas.Controllers
+{
+  public class PlanTrabajoController
+  {
+    /// <summary>
+    /// Sirve para traer los planes de trabajo de todos los proyectos
+    /// </summary>
+    /// <returns>Todos los planes de trabajo que existan en la base de datos</returns>
+    public string ObtenerPlanesTrabajo()
+    {
+      string rta = string.Empty;
+      DataTable dtSalida = clsPlanTrabajo.GetPlanesTrabajo();
+      if (dtSalida != null) //Se valida que la consulta de la base de datos venga con datos
+      {
+        dtSalida.TableName = "tabla";
+        rta = "{\"Head\":" + JsonConvert.SerializeObject(dtSalida, Formatting.Indented) + "}";
+      }
+      return rta;
+    }
+  }
+}
---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/VerificacionAnalisis/PlanTrabajo_ajax.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PlanTrabajo_ajax.aspx.cs" Inherits="AuditoriasCiudadanas.Views.VerificacionAnalisis.PlanTrabajo_ajax" %>
