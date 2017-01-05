d556105372484692d692ee2585d90798e0fe269d
Angelicamarianino <angelicamarianino@gmail.com>
2017-01-05 10:29:32 -0500

    errores comillas

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/ProyectosController.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/ProyectosController.cs
@@ -164,7 +164,7 @@ namespace AuditoriasCiudadanas.Controllers
                     tablaMonto += "</tr>";
                 }
                 tablaMonto += "</tbody></table></div></div>";
-                outTxt += "$(\"#divPresupuestoDet\").html(" + tablaMonto + ");";
+                outTxt += "$(\"#divPresupuestoDet\").html('" + tablaMonto + "');";
             }
             //--------------------------------------------------------------------
             if (dtPresupModif.Rows.Count > 0)
@@ -246,7 +246,7 @@ namespace AuditoriasCiudadanas.Controllers
                     Proyectos += "<li>" + dtProyectosOcad.Rows[i]["Proyecto"].ToString() + ". - " + dtProyectosOcad.Rows[i]["Localizacion"].ToString() + "</li>";
                 }
                 Proyectos += "</ul>";
-                outTxt += "$(\"#divPresOcadDet\").html(" + Proyectos + ");";
+                outTxt += "$(\"#divPresOcadDet\").html('" + Proyectos + "');";
             }
             //Ajustes
             if (dtAjustes.Rows.Count > 0)
