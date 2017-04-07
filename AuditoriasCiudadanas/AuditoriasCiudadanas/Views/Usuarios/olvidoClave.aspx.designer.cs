63821d498927da47d6aeea4670f04411b68cf1bf
dvillamizar <villamizardiana@gmail.com>
2017-04-07 11:09:00 -0500

    add_sin_seguimiento

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Usuarios/olvidoClave.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AuditoriasCiudadanas.Views.Usuarios {
+    
+    
+    public partial class olvidoClave {
+        
+        /// <summary>
+        /// Control divConfirmaEnvio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divConfirmaEnvio;
+        
+        /// <summary>
+        /// Control hdEnvio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden hdEnvio;
+        
+        /// <summary>
+        /// Control btnEnviaCodigoClave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor btnEnviaCodigoClave;
+    }
+}
