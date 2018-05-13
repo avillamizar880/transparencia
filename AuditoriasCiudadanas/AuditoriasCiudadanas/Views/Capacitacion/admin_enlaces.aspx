5831906265faffac81548b953f95da739fcb0abf
Diana villamizar <diroviar@hotmail.com>
2018-05-13 12:02:39 -0500

    recuperacion_enlaces

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Capacitacion/admin_enlaces.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="admin_enlaces.aspx.cs" Inherits="AuditoriasCiudadanas.Views.Capacitacion.admin_enlaces" %>
+<div class="container" id="divInfoEnlace">
+<input type="hidden" id="hdIdUsuario" runat="server" />
+    <h1>Creación de Guías y manuales</h1>
+    <div class="form-group">
+        <label for="ddlTipoRecurso" class="required">Guía/Manual</label>
+        <!-- departamento-->
+        <select class="form-control" id="ddlTipoRecurso">
+            <option value="" selected>Seleccione una opción</option>
+            <option value="1">Guía</option>
+            <option value="2">Manual</option>
+        </select>
+        <div id="error_ddlTipoRecurso" class="alert alert-danger alert-dismissible" hidden="hidden">Seleccione si es guía o manual</div>
+    </div>
+    <div class="form-group">
+        <label for="txtTitulo" class="required">Título</label>
+        <input type="text" class="form-control" id="txtTitulo">
+        <div id="error_txtTitulo" class="alert alert-danger alert-dismissible" hidden="hidden">Títulol no puede ser vacío</div>
+    </div>
+    <div class="form-group">
+        <label for="txtDescripcion" class="required">Descripción</label>
+        <input type="text" class="form-control" id="txtDescripcion">
+        <div id="error_txtDescripcion" class="alert alert-danger alert-dismissible" hidden="hidden">Descripción no puede ser vacío</div>
+    </div>
+    <div class="form-group">
+        <label for="txtEnlace" class="required">Link</label>
+        <input type="text" class="form-control" id="txtEnlace">
+        <div id="error_txtEnlace" class="alert alert-danger alert-dismissible" hidden="hidden">Link no puede ser vacío</div>
+    </div>
+
+      <!--BOTONERA-->
+    <div class="botonera text-center">
+        <div class="btn btn-primary"><a id="btnCrearEnlace" role="button">GUARDAR<span class="glyphicon glyphicon-chevron-right"></span></a></div>
+    </div>
+</div>
+<script type="text/javascript">
+   if ($(document).ready(function () {
+        $.getScript("../../Scripts/CapacitacionFunciones.js", function () {
+                $.getScript("../../Scripts/CapacitacionAcciones.js", function () {
+            });
+        });
+    }));
+</script>
---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Capacitacion/admin_enlaces_ajax.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AuditoriasCiudadanas.Views.Capacitacion
+{
+	public partial class admin_enlaces_ajax : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            string opcion = "";
+            string id_usuario = "";
+            string titulo = "";
+            string descripcion = "";
+            string enlace_url = "";
+            string outTxt = "";
+            int id_usuario_aux = 0;
+            string id_recurso = "";
+            int id_recurso_aux = 0;
+            string tipo_recurso = "";
+            int tipo_recurso_aux = 0;
+
+
+            if (HttpContext.Current.Request.HttpMethod == "POST")
+            {
+                NameValueCollection pColl = Request.Params;
+
+                if (pColl.AllKeys.Contains("tipo_recurso"))
+                {
+                    tipo_recurso = Request.Params.GetValues("tipo_recurso")[0].ToString();
+                }
+
+                if (pColl.AllKeys.Contains("opc"))
+                {
+                    opcion = Request.Params.GetValues("opc")[0].ToString();
+                }
+                if (pColl.AllKeys.Contains("id_recurso"))
+                {
+                    id_recurso = Request.Params.GetValues("id_recurso")[0].ToString();
+                }
+                if (pColl.AllKeys.Contains("id_usuario"))
+                {
+                    id_usuario = Request.Params.GetValues("id_usuario")[0].ToString();
+                }
+                if (pColl.AllKeys.Contains("titulo"))
+                {
+                    titulo = Request.Params.GetValues("titulo")[0].ToString();
+                }
+                if (pColl.AllKeys.Contains("desc"))
+                {
+                    descripcion = Request.Params.GetValues("desc")[0].ToString();
+                }
+                if (pColl.AllKeys.Contains("enlace_url"))
+                {
+                    enlace_url = Request.Params.GetValues("enlace_url")[0].ToString();
+                }
+                if (!string.IsNullOrEmpty(id_usuario))
+                {
+                    id_usuario_aux = Convert.ToInt16(id_usuario);
+                }
+                if (!string.IsNullOrEmpty(id_recurso))
+                {
+                    id_recurso_aux = Convert.ToInt16(id_recurso);
+                }
+                if (!string.IsNullOrEmpty(tipo_recurso))
+                {
+                    tipo_recurso_aux = Convert.ToInt16(tipo_recurso);
+                }
+
+                if (opcion.ToUpper().Equals("ADD"))
+                {
+                    AuditoriasCiudadanas.Controllers.CapacitacionController datosUsuario = new AuditoriasCiudadanas.Controllers.CapacitacionController();
+                    outTxt = datosUsuario.addRecursoMultimedia(tipo_recurso_aux, titulo, descripcion, enlace_url, id_usuario_aux);
+                }
+                else if (opcion.ToUpper().Equals("DEL"))
+                {
+                    AuditoriasCiudadanas.Controllers.CapacitacionController datosUsuario = new AuditoriasCiudadanas.Controllers.CapacitacionController();
+                    outTxt = datosUsuario.delRecursoMultimedia(id_recurso_aux, id_usuario_aux);
+                }
+                else if (opcion.ToUpper().Equals("MOD"))
+                {
+                    AuditoriasCiudadanas.Controllers.CapacitacionController datosUsuario = new AuditoriasCiudadanas.Controllers.CapacitacionController();
+                    outTxt = datosUsuario.modRecursoMultimedia(id_recurso_aux, titulo, descripcion, enlace_url, id_usuario_aux);
+                }
+
+
+                Response.Write(outTxt);
+                Response.End();
+
+            }
+        }
+	}
+}
