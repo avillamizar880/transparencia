37e24e0a51fc6ab8fc5ac63dd19bee899d6d5a4d
Victor Julio Ramirez Duran <vjramirez@gmail.com>
2018-07-12 22:49:20 -0500

    Configuración de tarea cron para generar los reportes de las ETLS.

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Startup.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Hangfire;
+using Microsoft.Owin;
 using Owin;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(AuditoriasCiudadanas.Startup))]
 namespace AuditoriasCiudadanas
@@ -8,6 +10,12 @@ namespace AuditoriasCiudadanas
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
             app.MapSignalR();
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Transparencia");
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate("ReportesXLS", () => Console.Write("Powerful!"), "00 23 * * *", TimeZoneInfo.Local);
         }
     }
 }
