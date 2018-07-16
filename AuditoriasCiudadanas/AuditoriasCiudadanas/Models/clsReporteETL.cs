2d218cf20024359b4d3fea2794e18f56041dce08
Victor Julio Ramirez Duran <vjramirez@gmail.com>
2018-07-15 23:29:08 -0500

    Se crea la funcionalidad para tareas programadas de Reportes de las ETLS.

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Models/clsReporteETL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AuditoriasCiudadanas.Models
+{
+    public class clsReporteETL
+    {
+        static string cadTransparencia = ConfigurationManager.ConnectionStrings["Transparencia"].ConnectionString;
+
+        public static List<DataTable> ObtDatos(string sp)
+        {
+            List<DataTable> Data = new List<DataTable>();
+            List<PaParams> parametros = new List<PaParams>();
+            Data = DbManagement.getDatos(sp, CommandType.StoredProcedure, cadTransparencia, parametros);
+            return Data;
+        }
+    }
+}
--- AuditoriasCiudadanas/AuditoriasCiudadanas/Startup.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Hangfire;
+using Microsoft.Owin;
 using Owin;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(AuditoriasCiudadanas.Startup))]
 namespace AuditoriasCiudadanas
@@ -7,6 +9,28 @@ namespace AuditoriasCiudadanas
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Transparencia");
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+
+            RecurringJob.AddOrUpdate("ReportesXLS",
+                () => App_Code.ReportesETLS.createReport("Reporte diario de Autoevaluacion auditores", "pa_obt_reporte_etl_sal12")
+                , "40 23 * * *"
+                , TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate("ReportesXLS1",
+                () => App_Code.ReportesETLS.createReport("Reporte diario de Evaluacion experiencia", "pa_obt_reporte_etl_sal13")
+                , "40 23 * * *"
+                , TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate("ReportesXLS2",
+                () => App_Code.ReportesETLS.createReport("Reporte diario de Valoracion proyecto", "pa_obt_reporte_etl_sal14")
+                , "40 23 * * *"
+                , TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate("ReportesXLS3",
+                () => App_Code.ReportesETLS.createReport("Reporte de Evaluacion posterior de proyectos", "pa_obt_reporte_etl_sal15")
+                , "40 23 * * *"
+                , TimeZoneInfo.Local);
         }
     }
 }
