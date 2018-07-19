1cfe844905e03faaf2b8e2426c16adc5e78a9650
Victor Julio Ramirez Duran <vjramirez@gmail.com>
2018-07-19 06:25:11 -0500

    Inserción en tabla de rutaReportes

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Models/clsReporteETL.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Models/clsReporteETL.cs
@@ -18,5 +18,30 @@ namespace AuditoriasCiudadanas.Models
             Data = DbManagement.getDatos(sp, CommandType.StoredProcedure, cadTransparencia, parametros);
             return Data;
         }
+
+        public static string addRutaReportes(int TipoReporte, string Ruta)
+        {
+            string outTxt = "";
+            string cod_error = "-1";
+            string mensaje_error = "@ERROR";
+            List<DataTable> Data = new List<DataTable>();
+            List<PaParams> parametros = new List<PaParams>();
+            parametros.Add(new PaParams("@tipo_reporte", SqlDbType.Int, TipoReporte, ParameterDirection.Input));
+            parametros.Add(new PaParams("@ruta", SqlDbType.VarChar, Ruta, ParameterDirection.Input, 4000));
+            parametros.Add(new PaParams("@cod_error", SqlDbType.Int, cod_error, ParameterDirection.Output));
+            parametros.Add(new PaParams("@mensaje_error", SqlDbType.VarChar, mensaje_error, ParameterDirection.Output));
+            Data = DbManagement.getDatos("dbo.pa_ins_rutaReportes", CommandType.StoredProcedure, cadTransparencia, parametros);
+            if (Data.Count > 1)
+            {
+                if (Data[1].Rows.Count > 0)
+                {
+                    cod_error = Data[1].Rows[0]["cod_error"].ToString();
+                    mensaje_error = Data[1].Rows[0]["mensaje_error"].ToString();
+                }
+            }
+
+            outTxt = cod_error + "<||>" + mensaje_error;
+            return outTxt;
+        }
     }
 }
