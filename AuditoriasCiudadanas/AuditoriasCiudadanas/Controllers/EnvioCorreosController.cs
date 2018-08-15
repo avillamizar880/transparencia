c58211d05e9d364942e552dbd06d7f1e19622b45
dbautista2015 <diana.bautista.otalora@gmail.com>
2018-08-15 17:21:59 -0500

    avance correo

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/EnvioCorreosController.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Controllers/EnvioCorreosController.cs
@@ -124,54 +124,7 @@ namespace AuditoriasCiudadanas.Controllers
       return outTxt;
 
     }
-
-    /// <summary>
-    /// Permite enviar un correo al usuario responsable de ejecutar una tarea
-    /// </summary>
-    /// <param name="idUsuario">Es el id del usuario responsable de la tarea</param>
-    /// <param name="fechaCreacion">Es la fecha en la cual se creó la tarea</param>
-    /// <param name="detalleTarea">Es el detalle de la tarea</param>
-    /// <returns>Devuelve un mensaje el cual indica si el correo se fue de forma exitosa</returns>
-    public String enviarCorreoTareaCreada(int idUsuario, string fechaCreacion, string detalleTarea)
-    {
-      string outTxt = "";
-      string mensaje = "";
-      string url_img = obtUrlLocal();
-      string email = string.Empty;
-      UsuariosController usuarioController = new UsuariosController();
-      DataTable dtUsuarioEnviarCorreo = usuarioController.obtDatosUsuario(idUsuario);
-      if (dtUsuarioEnviarCorreo.Rows.Count > 0 && dtUsuarioEnviarCorreo.Columns.Count>=3)
-      {
-        email = DBNull.Value != dtUsuarioEnviarCorreo.Rows[0].ItemArray[2] ? dtUsuarioEnviarCorreo.Rows[0].ItemArray[2].ToString() : string.Empty;
-        List <DataTable> listaInfo = new List<DataTable>();
-        listaInfo = Models.clsEnvioCorreos.obtCuentaCorreo(1);
-        DataTable dtConfig = listaInfo[0];
-        if (dtConfig.Rows.Count >= 1)
-        {
-          mensaje += "<html>";
-          mensaje += "<head>";
-          mensaje += "<title>Auditorias Ciudadanas - Notificaciones</title>";
-          mensaje += "<style>p {color:#fff;font-family:Tahoma, Geneva, sans-serif;font-size:16px;} h1 {font-family:Tahoma, Geneva, sans-serif;}";
-          mensaje += "</style>";
-          mensaje += "</head>";
-          mensaje += "<body style=\"font-family:Tahoma, Geneva, sans-serif\">";
-          mensaje += "<table style=\"color:#fff;background-color:#00A69C; width:600px;  margin:0 auto; padding:25px 0px\">";
-          mensaje += "<tr><td style=\"width:200px\"><img  src=\"" + url_img + "/Content/img/iconEmail6.gif\" width=\"30%\" alt=\"Verifica tu cuenta\"/></td>";
-          mensaje += "<td style=\"text-align:center\"><h1>Nueva Tarea</h1>";
-          mensaje += "<img src=\"" + url_img + "/Content/img/iconEmail6_1.gif\" alt=\"Icono de verificar cuenta\"/>";
-          mensaje += "<p style=\"width:60%; margin:0 auto; text-align:center\">Usted se le ha asignado como responsable de una nueva tarea ";
-          mensaje += "tenga en cuenta la siguiente información:\nDetalle de la tarea:"+ detalleTarea + "\nFecha de creación:"+ fechaCreacion + "</p><br />";
-          mensaje += "</td></tr></table>";
-          mensaje += "</body></html>";
-          outTxt = App_Code.CorreoUtilidad.envCorreoNet(mensaje, email, null, null, "Transparencia por Colombia: Nueva tarea", dtConfig);
-        }
-        else outTxt = "-1<||>Configuracion de correo inválida";
-      }
-      else outTxt = "-1<||>Usuario inválido";
-      return outTxt;
-    }
-
-
+        
     public String notificaCredencialesCorreo(string email, string idUsuario, string id_perfil)
     {
       string outTxt = "";
@@ -429,6 +382,99 @@ namespace AuditoriasCiudadanas.Controllers
       return outTxt;
 
     }
+
+    /// <summary>
+    /// Permite enviar un correo al usuario responsable de ejecutar una tarea
+    /// </summary>
+    /// <param name="idUsuario">Es el id del usuario responsable de la tarea</param>
+    /// <param name="fechaCreacion">Es la fecha en la cual se creó la tarea</param>
+    /// <param name="detalleTarea">Es el detalle de la tarea</param>
+    /// <returns>Devuelve un mensaje el cual indica si el correo se fue de forma exitosa</returns>
+    public String enviarCorreoTareaCreada(int idUsuario, string fechaCreacion, string detalleTarea)
+    {
+      string outTxt = "";
+      string mensaje = "";
+      string url_img = obtUrlLocal();
+      string email = string.Empty;
+      UsuariosController usuarioController = new UsuariosController();
+      DataTable dtUsuarioEnviarCorreo = usuarioController.obtDatosUsuario(idUsuario);
+      if (dtUsuarioEnviarCorreo.Rows.Count > 0 && dtUsuarioEnviarCorreo.Columns.Count >= 3)
+      {
+        email = DBNull.Value != dtUsuarioEnviarCorreo.Rows[0].ItemArray[2] ? dtUsuarioEnviarCorreo.Rows[0].ItemArray[2].ToString() : string.Empty;
+        List<DataTable> listaInfo = new List<DataTable>();
+        listaInfo = Models.clsEnvioCorreos.obtCuentaCorreo(1);
+        DataTable dtConfig = listaInfo[0];
+        if (dtConfig.Rows.Count >= 1)
+        {
+          mensaje += "<html>";
+          mensaje += "<head>";
+          mensaje += "<title>Auditorias Ciudadanas - Notificaciones</title>";
+          mensaje += "<style>p {color:#fff;font-family:Tahoma, Geneva, sans-serif;font-size:16px;} h1 {font-family:Tahoma, Geneva, sans-serif;}";
+          mensaje += "</style>";
+          mensaje += "</head>";
+          mensaje += "<body style=\"font-family:Tahoma, Geneva, sans-serif\">";
+          mensaje += "<table style=\"color:#fff;background-color:#8CBE43; width:600px;  margin:0 auto; padding:25px 0px\">";
+          mensaje += "<tr><td style=\"width:200px\"><img  src=\"" + url_img + "/Content/img/iconEmail1.gif\" width=\"100%\" alt=\"Nueva Tarea\"/></td>";
+          mensaje += "<td style=\"text-align:center\"><h1>Tienes una nueva tarea en tu grupo auditor</h1>";
+          mensaje += "<p style=\"width:60%; margin:0 auto; text-align:center\">Visita de campo<br /> " + fechaCreacion + "<br />Hora 3:00 p.m.</p><br /> ";
+          mensaje += "tenga en cuenta la siguiente información:\nDetalle de la tarea:" + detalleTarea + "\nFecha de creación:" + fechaCreacion + "</p><br />";
+          //Ejemplo de href para enviar a detalle tarea.
+          mensaje += "<a href=\"" + url_img + "/Views/Usuarios/verificaCuentaCorreo?keyUsu=" + idUsuario + "\"style =\"background-color:#2AA7DF; border-bottom:3px solid #278CB8; padding:5px 25px; color:#fff; font-weight:bold\">VER TAREA</a>";
+          mensaje += "</td></tr></table>";
+          mensaje += "</body></html>";
+          outTxt = App_Code.CorreoUtilidad.envCorreoNet(mensaje, email, null, null, "Transparencia por Colombia: Nueva tarea", dtConfig);
+        }
+        else outTxt = "-1<||>Configuracion de correo inválida";
+      }
+      else outTxt = "-1<||>Usuario inválido";
+      return outTxt;
+    }
+    /// <summary>
+    /// Permite enviar un correo al usuario responsable un día antes de vencer la tarea
+    /// </summary>
+    /// <param name=""></param>
+    /// <param name=""></param>
+    /// <param name=""></param>
+    /// <returns></returns>
+    //public String enviarCorreoVencimientoTarea(int idUsuario, string fechaCreacion, string detalleTarea)
+    //{
+    //  string outTxt = "";
+    //  string mensaje = "";
+    //  string url_img = obtUrlLocal();
+    //  string email = string.Empty;
+    //  UsuariosController usuarioController = new UsuariosController();
+    //  DataTable dtUsuarioEnviarCorreo = usuarioController.obtDatosUsuario(idUsuario);
+    //  if (dtUsuarioEnviarCorreo.Rows.Count > 0 && dtUsuarioEnviarCorreo.Columns.Count >= 3)
+    //  {
+    //    email = DBNull.Value != dtUsuarioEnviarCorreo.Rows[0].ItemArray[2] ? dtUsuarioEnviarCorreo.Rows[0].ItemArray[2].ToString() : string.Empty;
+    //    List<DataTable> listaInfo = new List<DataTable>();
+    //    listaInfo = Models.clsEnvioCorreos.obtCuentaCorreo(1);
+    //    DataTable dtConfig = listaInfo[0];
+    //    if (dtConfig.Rows.Count >= 1)
+    //    {
+    //      mensaje += "<html>";
+    //      mensaje += "<head>";
+    //      mensaje += "<title>Auditorias Ciudadanas - Notificaciones</title>";
+    //      mensaje += "<style>p {color:#fff;font-family:Tahoma, Geneva, sans-serif;font-size:16px;} h1 {font-family:Tahoma, Geneva, sans-serif;}";
+    //      mensaje += "</style>";
+    //      mensaje += "</head>";
+    //      mensaje += "<body style=\"font-family:Tahoma, Geneva, sans-serif\">";
+    //      mensaje += "<table style=\"color:#fff;background-color:#8CBE43; width:600px;  margin:0 auto; padding:25px 0px\">";
+    //      mensaje += "<tr><td style=\"width:200px\"><img  src=\"" + url_img + "/Content/img/iconEmail1.gif\" width=\"100%\" alt=\"Nueva Tarea\"/></td>";
+    //      mensaje += "<td style=\"text-align:center\"><h1>Tienes una nueva tarea en tu grupo auditor</h1>";
+    //      mensaje += "<p style=\"width:60%; margin:0 auto; text-align:center\">Visita de campo<br /> " + fechaCreacion + "<br />Hora 3:00 p.m.</p><br /> ";
+    //      mensaje += "tenga en cuenta la siguiente información:\nDetalle de la tarea:" + detalleTarea + "\nFecha de creación:" + fechaCreacion + "</p><br />";
+    //      //Ejemplo de href para enviar a detalle tarea.
+    //      mensaje += "<a href=\"" + url_img + "/Views/Usuarios/verificaCuentaCorreo?keyUsu=" + idUsuario + "\"style =\"background-color:#2AA7DF; border-bottom:3px solid #278CB8; padding:5px 25px; color:#fff; font-weight:bold\">VER TAREA</a>";
+    //      mensaje += "</td></tr></table>";
+    //      mensaje += "</body></html>";
+    //      outTxt = App_Code.CorreoUtilidad.envCorreoNet(mensaje, email, null, null, "Transparencia por Colombia: Nueva tarea", dtConfig);
+    //    }
+    //    else outTxt = "-1<||>Configuracion de correo inválida";
+    //  }
+    //  else outTxt = "-1<||>Usuario inválido";
+    //  return outTxt;
+    //}
 
   }
 
