432342c679a5b85c0fba10748314d592b843cc68
Usuario <Usuario@JORGE-LAPTOP-PC>
2018-08-21 19:18:26 -0500

    elim_promp_calendario

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Scripts/calendario.js
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Scripts/calendario.js
@@ -137,18 +137,6 @@ function MostrarCalendario() {
                                     allDaySlot: true,
                                     selectHelper: true,
                                     select: function (start, end, allDay) {
-                                        var title = prompt('Event Title:');
-                                        if (title) {
-                                            calendar.fullCalendar('renderEvent',
-                                                {
-                                                    title: title,
-                                                    start: start,
-                                                    end: end,
-                                                    allDay: allDay
-                                                },
-                                                true // make the event "stick"
-                                            );
-                                        }
                                         calendar.fullCalendar('unselect');
                                     },
                                     droppable: true, // this allows things to be dropped onto the calendar !!!
