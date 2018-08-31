a3d2decc75642c0352d691b109eda01e55ea08e4
Diana Bautista <diana.bautista.otalora@gmail.com>
2018-08-31 07:29:59 -0500

    Ajuste incidente 11

--- AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Administracion/CategoriaAuditorImagesActualizar_ajax.aspx.cs
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Administracion/CategoriaAuditorImagesActualizar_ajax.aspx.cs
@@ -25,58 +25,6 @@ namespace AuditoriasCiudadanas.Views.Administracion
                 break;
             }
       }
-      //string idTipoAuditor = string.Empty;
-      //string nombre = string.Empty;
-      //string descrip = string.Empty;
-      //string idUsuario = string.Empty;
-      //string rutaImagen = string.Empty;
-      //string limiteinferior = string.Empty;
-      //string limitesuperior = string.Empty;
-      //string cod_error = string.Empty;
-      //string msg_error = string.Empty;
-      //string sal = string.Empty;
-      //try
-      //{
-      //  if (HttpContext.Current.Request.HttpMethod == "POST")
-      //  {
-      //    NameValueCollection pColl = Request.Params;
-      //    if (pColl.AllKeys.Contains("idTipoAuditor")) idTipoAuditor = Request.Params.GetValues("idTipoAuditor")[0].ToString() == string.Empty ? "0" : Request.Params.GetValues("idTipoAuditor")[0].ToString();
-      //    if (pColl.AllKeys.Contains("nombre")) nombre = Request.Params.GetValues("nombre")[0].ToString();
-      //    if (pColl.AllKeys.Contains("descrip")) descrip = Request.Params.GetValues("descrip")[0].ToString();
-      //    if (pColl.AllKeys.Contains("idUsuario")) idUsuario = Request.Params.GetValues("idUsuario")[0].ToString();
-      //    if (pColl.AllKeys.Contains("rutaImagen")) rutaImagen = Request.Params.GetValues("rutaImagen")[0].ToString();
-      //    if (pColl.AllKeys.Contains("limiteinferior")) limiteinferior = Request.Params.GetValues("limiteinferior")[0].ToString();
-      //    if (pColl.AllKeys.Contains("limitesuperior")) limitesuperior = Request.Params.GetValues("limitesuperior")[0].ToString();
-      //    Response.AddHeader("Vary", "Accept");
-      //    try
-      //    {
-      //      if (Request["HTTP_ACCEPT"].Contains("application/json")) Response.ContentType = "application/json";
-      //      else Response.ContentType = "text/plain";
-      //    }
-      //    catch
-      //    {
-      //      Response.ContentType = "text/plain";
-      //    }
-      //    Controllers.AuditoriasController datos = new Controllers.AuditoriasController();
-      //    sal = datos.GuardarTipoAuditoria(idTipoAuditor + '*' + nombre + '*' + descrip + '*' + rutaImagen + '*' + limiteinferior + '*' + limitesuperior);
-      //    string[] separador = new string[] { "<||>" };
-      //    var result = sal.Split(separador, StringSplitOptions.None);
-      //    cod_error = result[0];
-      //    msg_error = result[1];
-      //    DataTable dterrores = new DataTable();
-      //    dterrores.Columns.Add("cod_error", typeof(string));
-      //    dterrores.Columns.Add("msg_error", typeof(string));
-      //    dterrores.Rows.Add(cod_error, msg_error);
-      //    dterrores.TableName = "tabla";
-      //    sal = "{\"Head\":" + JsonConvert.SerializeObject(dterrores) + "}";
-      //    Response.Write(sal);
-      //  }
-
-      //}
-      //catch (Exception exp)
-      //{
-      //  Response.Write(exp.Message);
-      //}
     }
   }
 }
