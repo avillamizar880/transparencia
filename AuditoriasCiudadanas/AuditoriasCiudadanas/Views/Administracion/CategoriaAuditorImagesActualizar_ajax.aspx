5052ad85f1fea84b2be05d0daf757896c939d802
Victor Julio Ramirez Duran <vjramirez@gmail.com>
2018-09-07 23:56:50 +0200

    Corrección de migración master a desarrollo

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Administracion/CategoriaAuditorImagesActualizar_ajax.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CategoriaAuditorImagesActualizar_ajax.aspx.cs" Inherits="AuditoriasCiudadanas.Views.Administracion.CategoriaAuditorImagesActualizar_ajax" %>
---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas/Views/Administracion/CategoriaAuditorImagesActualizar_ajax.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Configuration;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace AuditoriasCiudadanas.Views.Administracion
+{
+  public partial class CategoriaAuditorImagesActualizar_ajax : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Controllers.AuditoriasController datos = new Controllers.AuditoriasController();
+      if (Request.Form != null)
+      {
+        for (var i = 0; i < Request.Form.AllKeys.Length; i++)
+          if (Request.Form.AllKeys[i] != null)
+            switch (Request.Form.AllKeys[i].ToString().ToUpper())
+            {
+              case "ACTUALIZARREGISTROAUDITOR":
+                Response.Write(datos.GuardarTipoAuditoria(Request.Form[i]));
+                break;
+            }
+      }
+    }
+  }
+}
