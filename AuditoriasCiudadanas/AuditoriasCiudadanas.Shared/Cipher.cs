aad0a693bd8059ed4d9454ccab2651270a76bc8a
Jorge Ramírez <jramirezdev@outlook.com>
2018-09-16 09:53:01 -0500

    Agregado proyecto .net standard para funcionalidades compartidas entre la app movil y el api

---
+++ AuditoriasCiudadanas/AuditoriasCiudadanas.Shared/Cipher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuditoriasCiudadanas.Shared
+{
+    public static class Cipher
+    {
+        public static string SHA256Encripta(string input)
+        {
+            var provider = new SHA256CryptoServiceProvider();
+
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hashedBytes = provider.ComputeHash(inputBytes);
+
+            var output = new StringBuilder();
+
+            foreach (var t in hashedBytes)
+                output.Append(t.ToString("x2").ToLower());
+
+            return output.ToString();
+        }
+    }
+}
